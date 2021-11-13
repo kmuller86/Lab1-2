55e79fc73975fa4cac0f255b6687646cc61e4fcb
Mikołaj <mikolaj.sidor@o2.pl>
2021-11-13 10:01:04 +0100

    first change in cw 1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
-//        Użyć każdej z opcji
+//        Użyć każdej z opcjixddd
 
     }
 }
