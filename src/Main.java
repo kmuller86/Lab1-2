d10c5114eeacb3c6e7730287943cd09d36e15fad
Mikołaj <mikolaj.sidor@o2.pl>
2021-11-18 18:53:31 +0100

    Zadanie 3 koniec

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,46 @@ public class Main {
         *   wynik wyświetlić w konsoli programu
         */
 
+        Scanner scan = new Scanner(System.in);
 
+        System.out.print("Poodaj Imie: ");
+        String imie = scan.next();
+
+        System.out.print("Poodaj Nazwisko: ");
+        String nazwisko = scan.next();
+
+        System.out.print("wiek: ");
+        int wiek = scan.nextInt();
+
+        System.out.print("Podaj nr indeksu: ");
+        int nrIndexu = scan.nextInt();
+
+
+        System.out.printf("%s ,%s \n", imie , nazwisko);
+        System.out.println("wiek: "+ wiek);
+        System.out.println("nr indeksu: "+nrIndexu);
+
+
+        /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
+         *   wynik wyświetlić w konsoli programu
+         */
+
+        System.out.print("Pierwsza liczba do obliczeń: ");
+        int z1 = scan.nextInt();
+
+        System.out.print("Druga liczba do obliczeń: ");
+        int z2 = scan.nextInt();
+
+        System.out.println("wynikiem dodawania jest");
+        System.out.println(z1+z2);
+        System.out.println("wynikiem odejmowania jest");
+        System.out.println(z1-z2);
+        System.out.println("wynikiem mnożenia jest");
+        System.out.println(z1*z2);
+        System.out.println("wynikiem dzielenia jest");
+        System.out.println(z2/z1);
+        System.out.println("wynikiem modulo jest");
+        System.out.println(z1%z2);
+        System.out.println(z1%z2);
     }
 }
