e563bb7c1ce472d9abeef1138dc735c533698b51
Mikołaj <mikolaj.sidor@o2.pl>
2021-11-18 18:05:55 +0100

    Zadanie 1 koniec

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcjixddddddd
-
+        System.out.print("Pisanie tekstu bez nowej linijki:D\n");
+        System.out.println("Pisanie tesktu i ustawienie kursora w drugiej linijce");
+        System.out.print("%s – 1interpretuje argument jako łańcuch znaków\n%d – 2interpretuje argument jako liczbę całkowitą\n%f – 3interpretuje argument jako liczbę zmiennoprzecinkową\n%b – 4interpretuje argument jako wartość logiczną\n%n – 5interpretuje argument jako znak nowej linii\n");
+        System.out.printf("Teraz %s zacznie się zabawa z liczbami zmiennoprzecinkowymi jak np. %f\n","dopiero ",1.52554);
     }
 }
