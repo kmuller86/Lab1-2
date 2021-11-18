8705035aeda82b6a7b9875e320c5b82edb3d79cb
Mikołaj <mikolaj.sidor@o2.pl>
2021-11-18 18:27:34 +0100

    Zadanie 2 koniec

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -11,12 +9,48 @@ public class Main {
         /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
 
         /*
-        *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
-        *    mnożenia, dziealenia i modulo na zmiennych:
-        *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
-        *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
-        *  */
+         *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
+         *    mnożenia, dziealenia i modulo na zmiennych:
+         *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
+         *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
+         *  */
+
+        byte male = 127;
+        short wieksze = 32767;
+        int wystarczajace = 2147483647;
+        long Ogromne = 1841818185;
+        String imie = "Mikołaj";
+        double urodziny = 2.04;
+        boolean cokolwiekzawszetruefalse = true;
+
+        System.out.println(male);
+        System.out.println(wieksze);
+        System.out.println(wystarczajace);
+        System.out.println(Ogromne);
+        System.out.println(imie);
+        System.out.println(urodziny);
+        System.out.println(cokolwiekzawszetruefalse);
 
 
+        //cw2
+
+        int liczbaA = 100;
+        int liczbaB = 10;
+
+        double liczbaX = 125.125;
+        double liczbaY = 5.125;
+
+
+        System.out.println(liczbaA + liczbaB);
+        System.out.println(liczbaA - liczbaB);
+        System.out.println(liczbaA * liczbaB);
+        System.out.println(liczbaA / liczbaB);
+        System.out.println(liczbaB % liczbaA);
+
+        System.out.println(liczbaX + liczbaY);
+        System.out.println(liczbaX - liczbaY);
+        System.out.println(liczbaX * liczbaY);
+        System.out.println(liczbaY / liczbaX);
+        System.out.println(liczbaY % liczbaX);
     }
 }
