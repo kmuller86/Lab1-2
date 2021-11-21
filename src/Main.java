929f4110ee3f041ed47452f16be972aed4fab3e2
Mikołaj <mikolaj.sidor@o2.pl>
2021-11-21 10:28:01 +0100

    Zadanie 4 koniec

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+
 
 public class Main {
 
@@ -7,6 +7,19 @@ public class Main {
         /* ćw.4 */
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
+
+        boolean a = true;
+        boolean b = false;
+        boolean c = false;
+        int x = 5;
+        int y = 30;
+        int z = 18;
+
+        System.out.println("x == z(równość) : " + (x == z));
+        System.out.println("x != y(różny) : " + (x != y));
+        System.out.println("x < y(wiekszy, mniejszy)  : " + (x < y));
+        System.out.println("x >= x(wiekszy/mniejszy równy) " + (x >= y));
+        System.out.println("!(zaprzeczenie) x>y ||(lub) a &&(i) b : " + !((x > y) || a && c && b));
 
 
     }
